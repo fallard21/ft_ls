5c8fc71b0ad6a064331814be53c1c49ad5b2b251
fallard21 <bassirov@yandex.ru>
2020-08-16 01:24:11 +0300

    normed

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: fallard <fallard@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 21:03:52 by fallard           #+#    #+#             */
-/*   Updated: 2020/08/16 01:17:35 by fallard          ###   ########.fr       */
+/*   Updated: 2020/08/16 01:23:39 by fallard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,20 @@
 # define FT_LS_H
 
 # include "libft.h"
-# include <unistd.h>
 # include <sys/types.h>
-# include <sys/stat.h>	// stat
-# include <dirent.h>		// readdir
-# include <errno.h>		// perror
+# include <sys/stat.h>
+# include <dirent.h>
+# include <errno.h>
 # include <sys/xattr.h>	// getxattr
-# include <pwd.h>		// getpwuid
-# include <grp.h>		// getgrid
-# include <time.h>		// ctime
+# include <pwd.h>
+# include <grp.h>
+# include <time.h>
 # include <stdio.h>
 # include <fcntl.h>
 # include <linux/limits.h>
 # include <unistd.h>
-# include <sys/sysmacros.h>	//major minor
-
+# include <sys/sysmacros.h>
 # include <sys/ioctl.h>
-# include <termios.h>
 
 # define KEYS "lRartcuisSfog1"
 # define LRLINK "readlink"
@@ -49,14 +46,12 @@ typedef struct dirent	t_dir;
 typedef struct group	t_grp;
 typedef struct passwd	t_pw;
 typedef struct winsize	t_win;
-// -isScu
-// -AF ?
-// -go 
-// -1
 
-// -t - mtime
-// -c - ctime
-// -u - atime
+/*
+** -t - mtime
+** -c - ctime
+** -u - atime
+*/
 
 typedef struct		s_col
 {
@@ -98,15 +93,15 @@ typedef struct		s_ls
 	int				key_r;
 	int				key_t;
 
-	int				key_c;		// bonus
-	int				key_u;		// bonus
-	int				key_i;		// bonus
-	int				key_s;		// bonus
-	int				key_up_s;	// bonus
-	int				key_f;		// bonus
-	int				key_o;		// bonus
-	int				key_g;		// bonus
-	int				key_one;	// bonus 
+	int				key_c;
+	int				key_u;
+	int				key_i;
+	int				key_s;
+	int				key_up_s;
+	int				key_f;
+	int				key_o;
+	int				key_g;
+	int				key_one;
 
 	uint16_t		tty_width;
 
@@ -120,89 +115,89 @@ typedef struct		s_ls
 
 typedef struct		s_data
 {
-	t_file		*head;
-	t_col		col;
-	char		path[LSPATH];
-	int			spec_file;
-	int			size;
-	int			*width;
+	t_file			*head;
+	t_col			col;
+	char			path[LSPATH];
+	int				spec_file;
+	int				size;
+	int				*width;
 }					t_data;
 
 /*
 ** >----------------< PARSING ARGUMENTS >----------------<
 */
-int		parse_args_to_keys(t_ls *ls, int ac, char **av);
-void	parse_args_to_file(t_ls *ls, int ac, char **av);
-void	update_keys(t_ls *ls, char *del, int key);
-int		init_keys(t_ls *ls, char *argv);
-int		find_key(char key);
+int					parse_args_to_keys(t_ls *ls, int ac, char **av);
+void				parse_args_to_file(t_ls *ls, int ac, char **av);
+void				update_keys(t_ls *ls, char *del, int key);
+int					init_keys(t_ls *ls, char *argv);
+int					find_key(char key);
 
 /*
 ** >----------------< GET DIR INFO >----------------<
 */
-t_data	get_data(t_ls *ls, t_file *head, char *path, char *dir);
-t_file	*get_dir_files(t_ls *ls, char *fpath);
-t_file	*new_file(t_ls *ls, char *path, char *name);
-void	get_lstat(t_ls *ls, t_file *tmp, char *fpath);
-void	get_symbolic_link(t_file *tmp, char *fpath);
-int		list_size(t_file *head);
-int		*get_width_arr(t_file *head);
-void	fix_path(char *path);
-void	init_path(char *add, char *path, char *dir, int flag);
-void	display_path(t_ls *ls, char *fpath);
+t_data				get_data(t_ls *ls, t_file *head, char *path, char *dir);
+t_file				*get_dir_files(t_ls *ls, char *fpath);
+t_file				*new_file(t_ls *ls, char *path, char *name);
+void				get_lstat(t_ls *ls, t_file *tmp, char *fpath);
+void				get_symbolic_link(t_file *tmp, char *fpath);
+int					list_size(t_file *head);
+int					*get_width_arr(t_file *head);
+void				fix_path(char *path);
+void				init_path(char *add, char *path, char *dir, int flag);
+void				display_path(t_ls *ls, char *fpath);
 
 /*
 ** >----------------< D0 LS >----------------<
 */
-void	do_ls(t_ls *ls);
-void	display_arguments(t_ls *ls, t_file *root);
-void	display_dir(t_ls *ls, char *path, char *name);
+void				do_ls(t_ls *ls);
+void				display_arguments(t_ls *ls, t_file *root);
+void				display_dir(t_ls *ls, char *path, char *name);
 
 /*
 ** >----------------< SORTING >----------------<
 */
-t_file			*sort(t_ls *ls, t_file *head);
-t_file			*sort_list(int (*cmp)(t_file*, t_file*), t_file *head);
-t_file			*reverse_list(t_file *head);
-int				cmp_size(t_file *left, t_file *right);
-int				cmp_name(t_file *left, t_file *right);
-int				cmp_atime(t_file *left, t_file *right);
-int				cmp_ctime(t_file *left, t_file *right);
-int				cmp_mtime(t_file *left, t_file *right);
+t_file				*sort(t_ls *ls, t_file *head);
+t_file				*sort_list(int (*cmp)(t_file*, t_file*), t_file *head);
+t_file				*reverse_list(t_file *head);
+int					cmp_size(t_file *left, t_file *right);
+int					cmp_name(t_file *left, t_file *right);
+int					cmp_atime(t_file *left, t_file *right);
+int					cmp_ctime(t_file *left, t_file *right);
+int					cmp_mtime(t_file *left, t_file *right);
 
 /*
 ** >----------------< DISPLAY >----------------<
 */
-void	display_files(t_ls *ls, t_data data);
-void	display_key_l(t_ls *ls, t_data data);
-void	display_one_column(t_ls *ls, t_data *data);
-void	display_total(t_ls *ls, t_file *head);
-void	display_error(char *file, int flag);
-void	error_exit(t_ls *ls, char *file, int flag);
+void				display_files(t_ls *ls, t_data data);
+void				display_key_l(t_ls *ls, t_data data);
+void				display_one_column(t_ls *ls, t_data *data);
+void				display_total(t_ls *ls, t_file *head);
+void				display_error(char *file, int flag);
+void				error_exit(t_ls *ls, char *file, int flag);
 
 /*
 ** >----------------< COLUMNS DISPLAY >----------------<
 */
-void	display_column(t_ls *ls, t_data *data);
-void	print_column(t_ls *ls, t_data *data, t_col *col, int row);
-int		get_row(t_ls *ls, t_data *data);
+void				display_column(t_ls *ls, t_data *data);
+void				print_column(t_ls *ls, t_data *data, t_col *col, int row);
+int					get_row(t_ls *ls, t_data *data);
 
 /*
 ** >----------------< EXTENDED DISPLAY >----------------<
 */
-void	display_chmod(t_file *tmp);
-void	display_users(t_ls *ls, t_file *head, int *width);
-void	display_size(t_data data, t_file *f, int *width);
-void	display_time(t_ls *ls, t_file *tmp);
-void	display_name(t_ls *ls, t_file *current);
+void				display_chmod(t_file *tmp);
+void				display_users(t_ls *ls, t_file *head, int *width);
+void				display_size(t_data data, t_file *f, int *width);
+void				display_time(t_ls *ls, t_file *tmp);
+void				display_name(t_ls *ls, t_file *current);
 
 /*
 ** >----------------< MEMORY >----------------<
 */
-void	ft_exit(char *error);
-int		free_split(char ***str);
-void	free_list(t_file **head);
-int		free_data(t_data *data);
-int		free_col(t_col *col);
+void				ft_exit(char *error);
+int					free_split(char ***str);
+void				free_list(t_file **head);
+int					free_data(t_data *data);
+int					free_col(t_col *col);
 
 #endif
